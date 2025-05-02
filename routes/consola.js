4997418856588bb8435c10a27008d0cfdda61262
Omar Pachas <jesuspachas0403@gmail.com>
2025-05-02 02:07:13 -0500

    Rutas modificadas para implementar imagenes con multer

--- routes/consola.js
+++ routes/consola.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database'); 
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const db = require('../config/database');
 
-router.get('/', async (req, res) => {  // Ruta principal /
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/images/consolas')); //ruta donde se van a guardar las imagenes enviadas en el formulario
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname).toLowerCase());
+  }
+});
+const upload = multer({ storage: storage });
+
+//ruta para obtener el total de registros en home
+router.get('/', async (req, res) => {
   try {
-    // Obtener directamente el total de consolas
     const [result] = await db.query('SELECT COUNT(*) AS total FROM consolas');
-    res.render('home', { total: result[0].total });  // Pasamos el total directamente a la vista home
+    res.render('home', { total: result[0].total });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error al obtener la vista de inicio');
+    res.status(500).send('Error');
   }
 });
 
-
-router.get('/index', async (req, res) => {  // Ruta '/index'
+// Ruta para el litado de registros 
+router.get('/index', async (req, res) => {
   try {
     const query = `
       SELECT 
@@ -28,21 +42,21 @@ router.get('/index', async (req, res) => {  // Ruta '/index'
         C.sonido,
         C.peso,
         M.marca,
-        CA.categoria
+        CA.categoria,
+        C.imagen
       FROM consolas C
       INNER JOIN marcas M ON C.idmarca = M.idmarca
       INNER JOIN categorias CA ON C.idcategoria = CA.idcategoria
     `;
     const [consolas] = await db.query(query);
-    res.render('index', { consolas });  // Renderiza 'index.ejs' con la tabla de consolas
+    res.render('index', { consolas });
   } catch (error) {
     console.error(error);
     res.status(500).send('Error al obtener las consolas');
   }
 });
 
-
-//ruta para mostrar el formulario
+// Ruta para mostrar el formulario de registros
 router.get('/create', async (req, res) => {
   try {
     const [marcas] = await db.query("SELECT * FROM marcas");
@@ -54,19 +68,17 @@ router.get('/create', async (req, res) => {
   }
 });
 
-
-//Guardar nuevo registro
-router.post('/create', async (req, res) => {
+// Guardar nuevo registro
+router.post('/create', upload.single('imagen'), async (req, res) => {
   try {
     const { nombre, descripcion, precio, modelo, alanzamiento, almacenamiento, sonido, peso, idmarca, idcategoria } = req.body;
-    
+    const imagen = req.file ? req.file.filename : null;
     await db.query(
       `INSERT INTO consolas 
-        (nombre, descripcion, precio, modelo, alanzamiento, almacenamiento, sonido, peso, idmarca, idcategoria) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-      [nombre, descripcion, precio, modelo, alanzamiento, almacenamiento, sonido, peso, idmarca, idcategoria]
+        (nombre, descripcion, precio, modelo, alanzamiento, almacenamiento, sonido, peso, idmarca, idcategoria, imagen) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [nombre, descripcion, precio, modelo, alanzamiento, almacenamiento, sonido, peso, idmarca, idcategoria, imagen]
     );
-
     res.redirect('/index');
   } catch (error) {
     console.error(error);
@@ -74,26 +86,26 @@ router.post('/create', async (req, res) => {
   }
 });
 
-//Elimnar registro
+//Eliminar registro
 router.get('/delete/:id', async (req, res) => {
   try {
-    const [resultado] = await db.query("DELETE FROM consolas WHERE idconsola = ?", [req.params.id]);
+    await db.query("DELETE FROM consolas WHERE idconsola = ?", [req.params.id]);
     res.redirect('/index');
   } catch (error) {
     console.error(error);
   }
 });
 
+// Ruta para mostrar el formulario y editar
 router.get('/edit/:id', async (req, res) => {
   try {
     const [marcas] = await db.query("SELECT * FROM marcas");
     const [categorias] = await db.query("SELECT * FROM categorias");
     const [registro] = await db.query("SELECT * FROM consolas WHERE idconsola = ?", [req.params.id]);
-
     if (registro.length > 0) {
       res.render('edit', {
-        marcas: marcas,
-        categorias: categorias,
+        marcas,
+        categorias,
         consola: registro[0]
       });
     } else {
@@ -105,40 +117,45 @@ router.get('/edit/:id', async (req, res) => {
   }
 });
 
-router.post('/edit/:id', async (req, res) => {
-  try {
-    const {
-      idmarca,
-      idcategoria,
-      nombre,
-      modelo,
-      descripcion,
-      precio,
-      alanzamiento,
-      almacenamiento,
-      sonido,
-      peso
-    } = req.body;
 
+// Ruta para guardar las imágenes subidas
+const uploadPath = path.join(__dirname, 'public', 'images'); 
+
+router.post('/edit/:id', upload.single('imagen'), async (req, res) => {
+  try {
+    const { idmarca, idcategoria, nombre, modelo, descripcion, precio, alanzamiento, almacenamiento, sonido, peso, imagen } = req.body;
+    if (!imagen) {
+      const [oldConsola] = await db.query('SELECT imagen FROM consolas WHERE idconsola = ?', [req.params.id]);
+      imagen = oldConsola[0].imagen;
+    } else {
+      const [oldConsola] = await db.query('SELECT imagen FROM consolas WHERE idconsola = ?', [req.params.id]);
+      const oldImagePath = path.join(uploadPath, oldConsola[0].imagen);
+      if (fs.existsSync(oldImagePath)) {
+        fs.unlinkSync(oldImagePath); 
+      }
+    }
+
+    // Realizar la actualización
     await db.query(`
       UPDATE consolas 
       SET idmarca = ?, idcategoria = ?, nombre = ?, modelo = ?, descripcion = ?, 
-          precio = ?, alanzamiento = ?, almacenamiento = ?, sonido = ?, peso = ?
-      WHERE idconsola = ?
-    `, [
-      idmarca,
-      idcategoria,
-      nombre,
-      modelo,
-      descripcion,
-      precio,
-      alanzamiento,
-      almacenamiento,
-      sonido,
-      peso,
-      req.params.id
-    ]);
-
+          precio = ?, alanzamiento = ?, almacenamiento = ?, sonido = ?, peso = ?, imagen = ?
+      WHERE idconsola = ?`,
+      [
+        idmarca,
+        idcategoria,
+        nombre,
+        modelo,
+        descripcion,
+        precio,
+        alanzamiento,
+        almacenamiento,
+        sonido,
+        peso,
+        imagen,
+        req.params.id
+      ]
+    );
     res.redirect('/index');
   } catch (error) {
     console.error('Error al editar consola:', error);
@@ -146,10 +163,10 @@ router.post('/edit/:id', async (req, res) => {
   }
 });
 
+
 // Ruta para mostrar el catálogo
 router.get('/catalogo', async (req, res) => {
   try {
-    // Aquí podrías hacer una consulta SQL para obtener los productos o consolas del catálogo
     const query = `
       SELECT 
         C.idconsola,
@@ -162,19 +179,19 @@ router.get('/catalogo', async (req, res) => {
         C.sonido,
         C.peso,
         M.marca,
-        CA.categoria
+        CA.categoria,
+        C.imagen
       FROM consolas C
       INNER JOIN marcas M ON C.idmarca = M.idmarca
       INNER JOIN categorias CA ON C.idcategoria = CA.idcategoria
     `;
     
     const [consolas] = await db.query(query);
-    res.render('catalogo', { consolas });  // Renderiza la vista 'catalogo' y pasa la información de las consolas
+    res.render('catalogo', { consolas });
   } catch (error) {
     console.error(error);
     res.status(500).send('Error al obtener el catálogo');
   }
 });
-
 
 module.exports = router;
